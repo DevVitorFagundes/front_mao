36730587f41bfe223783f9bb88133b21bda90300
Vitor Fagundes <vitorfagundes575@gmail.com>
2024-09-29 21:33:35 -0300

    add changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Header } from "@/components/Header";
 import Home from '@/components/Home';
+import { Footer } from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Mão na roda",
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body>
         <Header />
         <Home />
+        <Footer />
         {children}
       </body>
     </html>
